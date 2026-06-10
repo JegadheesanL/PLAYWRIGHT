39d185614eace2fd9522e3af033a1abaceb4cd9a
JegadheesanL <l.jegadheesan@gmail.com>
2026-06-10 20:11:30 +0530

    modify

--- tests/facebooklogin.spec.js
+++ tests/facebooklogin.spec.js
@@ -3,4 +3,8 @@ test('facebook launch',async({page})=>{
     await page.goto('https://www.facebook.com/?_rdr')//open URL link in safari branch1
 
 })
+test('facebook launch',async({page})=>{
+    await page.goto('https://www.instagram.com')//open URL link in safari branch1
+
+})
 
