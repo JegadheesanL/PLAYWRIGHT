58d8a32fedca7f89e310b44fd667ac3f6a1d6e65
JegadheesanL <l.jegadheesan@gmail.com>
2026-06-10 22:52:11 +0530

    msg

--- tests/TextBox.spec.js
+++ tests/TextBox.spec.js
@@ -7,7 +7,7 @@ test('text box',async({page})=>{
     await page.locator('//textarea[@placeholder="Current Address"]').fill('trendnologies perungudi,kandanchavadi')
     await page.locator('//textarea[@id="permanentAddress"]').fill('Kumaran Nagar ,chennai')
     await page.waitForTimeout(3000)
-    await page.locator('//button[@id="submit"]').click
+    await page.locator('//button[@id="submit"]').check
     
 
     
