05329bc713ac52537802548fa79cfaf187d25bb0
JegadheesanL <l.jegadheesan@gmail.com>
2026-06-10 19:31:32 +0530

    modified

--- tests/facebooklogin.spec.js
+++ tests/facebooklogin.spec.js
@@ -1,6 +1,6 @@
 import{test}from '@playwright/test'
 test('facebook launch',async({page})=>{
-    await page.goto('https://www.facebook.com/?_rdr')//open URL link in chrome
+    await page.goto('https://www.facebook.com/?_rdr')//open URL link in chrome browser
 
 })
 
