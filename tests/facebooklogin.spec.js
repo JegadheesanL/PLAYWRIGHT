9fd97ce077adb68ffeac82c7cce4431bdafb047c
JegadheesanL <l.jegadheesan@gmail.com>
2026-06-11 17:41:48 +0530

    Update facebooklogin.spec.js

--- tests/facebooklogin.spec.js
+++ tests/facebooklogin.spec.js
@@ -2,7 +2,4 @@ import{test}from '@playwright/test'
 test('facebook launch',async({page})=>{
     await page.goto('https://www.facebook.com/?_rdr')//open URL link in chrome browser main
 })
-test('facebook launch',async({page})=>{
-    await page.goto('https://www.instagram.com')//open URL link in safari branch1
-})
 
