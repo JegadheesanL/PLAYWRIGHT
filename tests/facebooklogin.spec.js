06cc78dce651d2cf1072d87513fea3ecb4344e02
JegadheesanL <l.jegadheesan@gmail.com>
2026-06-12 17:20:03 +0530

    main

--- tests/facebooklogin.spec.js
+++ tests/facebooklogin.spec.js
@@ -1,6 +1,4 @@
 import{test}from '@playwright/test'
 test('facebook launch',async({page})=>{
     await page.goto('https://www.facebook.com/?_rdr')
-    await page.goto('https://www.facebook.com/?_rdr')
 })
-
